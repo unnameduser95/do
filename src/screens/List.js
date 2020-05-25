1d0a5d4ae6f986d63884fe10cedbd6aa259be9c9
unnameduser95 <unnameduser95@pm.me>
2020-05-24 19:30:32 -0700

    move lists screen

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 export default function List({ route }) {
   console.log(route.params.list);
 
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.heading} allowFontScaling={false}>{route.params.list.title}</Text>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: "#ffffff"
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 90,
+    marginHorizontal: 10,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+})
